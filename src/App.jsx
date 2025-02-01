eea86287904060c472c3cb4ae9eed1ccfc22db80
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2025-02-01 15:21:00 +0100

    Refactor App layout and styles, update button designs and background

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import Logo from './assets/pprime.png';
 
 function App() {
     return (
-        <div className='flex flex-col justify-center items-center h-screen -mt-36'>
-            <img src={Logo} alt="Pprime Logo" className='w-[650px]' />
-            <div className='-mt-20'>
-                <a href='http://192.168.1.6:5001' className='border-4 border-[#06402B] text-[#06402B] px-20 py-4 rounded-full m-2 font-semibold hover:bg-[#06402B] hover:text-white transition-all'>
+        <div className='flex flex-col justify-center items-center h-screen -mt-52'>
+            <img src={Logo} alt="Pprime Logo" className='w-[750px]' />
+            <div className=''>
+                <a href='http://192.168.1.6:5001' className='border-4 border-[#06402B] text-[#06402B] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
                     POil
                 </a>
-                <a href='http://192.168.1.6:5002' className='border-4 border-[#333A73] text-[#333A73] px-20 py-4 rounded-full m-2 font-semibold hover:bg-[#333A73] hover:text-white transition-all'>
+                <a href='http://192.168.1.6:5002' className='border-4 border-[#333A73]  text-[#333A73] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
                     PFish
                 </a>
-                <a href='http://192.168.1.6:5003' className='border-4 border-[#022213] text-[#022213] px-20 py-4 rounded-full m-2 font-semibold hover:bg-[#022213] hover:text-white transition-all'>
+                <a href='http://192.168.1.6:5003' className='border-4 border-[#022213]  text-[#022213] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
                     PBoiler
                 </a>
-                <a href='http://192.168.1.6:5004' className='border-4 border-[#0569FA] text-[#0569FA] px-20 py-4 rounded-full m-2 font-semibold hover:bg-[#0569FA] hover:text-white transition-all'>
+                <a href='http://192.168.1.6:5004' className='border-4 border-[#F4CE14]  text-[#F4CE14] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
                     PEnergy
                 </a>
-                <a href='http://192.168.1.6:5005' className='border-4 border-[#2E4769] text-[#2E4769] px-20 py-4 rounded-full m-2 font-semibold hover:bg-[#2E4769] hover:text-white transition-all'>
+                <a href='http://192.168.1.6:5005' className='border-4 border-[#2E4769]  text-[#2E4769] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
                     PWater
                 </a>
             </div>
@@ -25,4 +25,4 @@ function App() {
     )
 }
 
-export default App
+export default App;
