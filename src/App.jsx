6d58379ee06b90c75ced8bd8acc54d9c372f45e0
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2025-02-04 22:15:01 +0100

    Add Logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import Logo from './assets/pprime.png';
+import POil from './assets/POil.svg';
+import PFish from './assets/PFish.svg';
+import PBoiler from './assets/PBoiler.svg';
+import PEnergy from './assets/PEnergy.svg';
+import PWater from './assets/PWater.svg';
+import PSense from './assets/PSense.svg';
+import PDever from './assets/PDever.png';
 
 function App() {
     return (
-        <div className='flex flex-col justify-center items-center h-screen -mt-52'>
+        <div className='flex flex-col justify-center items-center h-screen -mt-32'>
             <img src={Logo} alt="Pprime Logo" className='w-[750px]' />
-            <div className=''>
-                <a href='http://192.168.1.6:5001' className='border-4 border-[#06402B] text-[#06402B] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
-                    POil
-                </a>
-                <a href='http://192.168.1.6:5002' className='border-4 border-[#333A73]  text-[#333A73] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
-                    PFish
-                </a>
-                <a href='http://192.168.1.6:5003' className='border-4 border-[#022213]  text-[#022213] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
-                    PBoiler
-                </a>
-                <a href='http://192.168.1.6:5004' className='border-4 border-[#F4CE14]  text-[#F4CE14] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
-                    PEnergy
-                </a>
-                <a href='http://192.168.1.6:5005' className='border-4 border-[#2E4769]  text-[#2E4769] bg-white text-xl px-20 py-10 rounded-3xl m-2 font-semibold transition-all'>
-                    PWater
-                </a>
+            <div className='flex flex-col justify-between items-center -mt-28'>
+                <div className='flex justify-baseline items-center'>
+                    <a href='http://192.168.1.10:8024' className='m-12 p-3 rounded-md'>
+                        <img src={PFish} alt="" width={150} />
+                    </a>
+                    <a href='http://192.168.1.10:8022' className='m-8 p-3 rounded-md'>
+                        <img src={PEnergy} alt="" width={170} />
+                    </a>
+                    <a href='http://192.168.1.10:8020' className='m-8 p-3 rounded-md'>
+                        <img src={PBoiler} alt="" width={150} />
+                    </a>
+                    <a href='http://192.168.1.10:8021' className='m-8 p-3 rounded-md'>
+                        <img src={POil} alt="" width={140} />
+                    </a>
+                </div>
+                <div className='flex justify-between items-center'>
+                    <a href='http://192.168.1.10:8023' className='m-8 p-3 rounded-md'>
+                        <img src={PWater} alt="" width={150} />
+                    </a>
+                    <a href='http://192.168.1.10:8026' className='m-8 p-3 rounded-md'>
+                        <img src={PSense} alt="" width={150} />
+                    </a>
+                    <a href='http://192.168.1.10:8025' className='m-8 p-3 rounded-md'>
+                        <img src={PDever} alt="" width={110} />
+                    </a>
+                </div>
             </div>
         </div>
     )
